deedc44f458d642e63d38288ec7ab0c092035aac
Devin <devthedevel@gmail.com>
2023-09-24 15:04:09 -0230

    Updated index page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'devthedevel';
+export const SITE_DESCRIPTION = 'Welcome to my little slice of the internet where I talk about my projects, things in the tech world, or just rant about things. Enjoy!';
