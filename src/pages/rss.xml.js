2b107e72c349985a8d9fe174db47c3a461a69580
Devin <40843522+devthedevel@users.noreply.github.com>
2024-01-11 19:10:48 -0330

    Fixing blogs for rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,12 +3,14 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const blogCollection = await getCollection('blog');
+    const blogPosts = Array.isArray(blogCollection) ? blogCollection : [];
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
+		items: blogPosts.map((post) => ({
 			...post.data,
 			link: `/blog/${post.slug}/`,
 		})),
