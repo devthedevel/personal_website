d8685a63e2389b2f71c1db857d6192050a69fc56
Devin <40843522+devthedevel@users.noreply.github.com>
2024-01-13 22:21:25 -0330

    Added astro plugin

--- prettier.config.js
+++ prettier.config.js
@@ -2,6 +2,15 @@
 const config = {
   singleQuote: true,
   jsxSingleQuote: true,
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
 };
 
 export default config;
